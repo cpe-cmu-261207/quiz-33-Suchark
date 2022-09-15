fc4a75aa1137ff32c1856303306ec93c8087ac9c
Your Name <you@example.com>
2022-09-15 15:20:51 +0700

    okkkkkkkkkkkkkkkk

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -5,7 +5,7 @@ export default function Reply(props) {
     <div>
       <div className="d-flex gap-2 my-2 ps-5">
         <img
-          src="/profileImages/puppy.jpg"
+          src={props.profileImg}
           width="48"
           height="48"
           className="rounded-circle"
@@ -16,29 +16,18 @@ export default function Reply(props) {
           style={{ backgroundColor: "#3A3B3C" }}
         >
           <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-            หมาน้อย
+            {props.username}
           </span>
           <br />
-          <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-        </div>
-      </div>
-      <div className="d-flex gap-2 my-2 ps-5">
-        <img
-          src="/profileImages/charliebrown.jpg"
-          width="48"
-          height="48"
-          className="rounded-circle"
-          style={{ objectFit: "cover" }}
-        />
-        <div
-          className="rounded rounded-3 p-2"
-          style={{ backgroundColor: "#3A3B3C" }}
-        >
-          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-            ไก่จา
-          </span>
-          <br />
-          <span style={{ color: "#E4E6EB" }}>หยุดก่อนอานนท์</span>
+          <span style={{ color: "#E4E6EB" }}>{props.content}</span>
+          <div
+            className={
+              props.likeNum > 0 ? "d-flex align-items-center gap-1" : "d-none"
+            }
+          >
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>{props.likeNum}</span>
+          </div>
         </div>
       </div>
     </div>
